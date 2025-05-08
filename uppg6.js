0546e0f4e522b42964cb5e9cf533dfdcb9f6cb81
Isak Nywall <nywall@pm.me>
2025-05-08 13:46:38 +0200

    feat: solve uppg6

--- uppg6.js
+++ uppg6.js
@@ -1,11 +1,8 @@
-
-
-function uppg6(){
-
-    // Skapa en array med namn som du döper till names
-    // Loopa igenom arrayen och skriv ut alla namn i konsolen
-
- 
+function uppg6() {
+  // Skapa en array med namn som du döper till names
+  // Loopa igenom arrayen och skriv ut alla namn i konsolen
+  const names = ["Isak", "Foo", "Bar"];
+  names.forEach((name) => console.log(name));
 }
 
 module.exports = { uppg6 };
