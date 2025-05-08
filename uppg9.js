c572b2c18c135338bc381e09d94a13b20478fabd
Isak Nywall <nywall@pm.me>
2025-05-08 14:01:24 +0200

    feat: solve uppg9

--- uppg9.js
+++ uppg9.js
@@ -1,14 +1,24 @@
+function uppg9() {
+  // skapa en funktion som heter sort och tar in en array av siffror som argument
+  // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
+  // i if-sattsen skriv ut i konsolen resultatet
+  // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
 
+  // anropa funktionen och skicka med en array som argument
 
-function uppg9 (){
-    // skapa en funktion som heter sort och tar in en array av siffror som argument
-    // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
-    // i if-sattsen skriv ut i konsolen resultatet
-    // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
+  function sort(numbers) {
+    numbers.forEach((number) => {
+      let label;
+      if (number % 2 === 0) {
+        label = "jämt";
+      } else {
+        label = "udda";
+      }
+      console.log(number, label);
+    });
+  }
 
-    // anropa funktionen och skicka med en array som argument
-
-  
+  sort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 100]);
 }
 
 module.exports = { uppg9 };
