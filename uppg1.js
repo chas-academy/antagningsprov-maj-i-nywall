76895120085ec0e2346bcc70a888f026f453fe5b
Isak Nywall <nywall@pm.me>
2025-05-08 13:36:44 +0200

    feat: solve uppg1

--- uppg1.js
+++ uppg1.js
@@ -1,12 +1,7 @@
+function uppg1() {
+  // Skriv ut texten "Hello World!" i konsolen
 
+  console.log("Hello World!");
+}
 
-function uppg1(){
-
-    // Skriv ut texten "Hello World!" i konsolen
-    
-
-    
-    }
-
-
-    module.exports = { uppg1 };
+module.exports = { uppg1 };
