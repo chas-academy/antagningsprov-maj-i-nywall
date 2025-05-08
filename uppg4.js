8381d2adcf63dc9d2f2ea24227b8d1ea88f6f5e6
Isak Nywall <nywall@pm.me>
2025-05-08 13:41:29 +0200

    feat: solve uppg4.js

--- uppg4.js
+++ uppg4.js
@@ -1,12 +1,10 @@
-
 function uppg4() {
-  
-    // deklarera ett objekt med namnet person
-    // objektet ska ha två properties, name och age
-    // tilldela age 25
-    // skriv ut med dotnotation åldern i konsolen
-
-
+  // deklarera ett objekt med namnet person
+  // objektet ska ha två properties, name och age
+  // tilldela age 25
+  // skriv ut med dotnotation åldern i konsolen
+  const person = { name: "foo", age: 25 };
+  console.log(person.age);
 }
 
 module.exports = { uppg4 };
