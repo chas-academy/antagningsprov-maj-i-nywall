9f9b464aa3329c7ff5d4e9981e06933248506201
Isak Nywall <nywall@pm.me>
2025-05-08 13:37:40 +0200

    feat: solve uppg2

--- uppg2.js
+++ uppg2.js
@@ -1,9 +1,8 @@
-
 function uppg2() {
-   // Skriv din kod här
-   // deklarera en variable med ditt namn och skriv ut det i konsolen
-
-
+  // Skriv din kod här
+  // deklarera en variable med ditt namn och skriv ut det i konsolen
+  const name = "Isak Nywall";
+  console.log(name);
 }
 
 module.exports = { uppg2 };
