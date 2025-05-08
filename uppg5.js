a400f6bc620c5c097f90672b995b1b35637c0f0c
Isak Nywall <nywall@pm.me>
2025-05-08 13:44:27 +0200

    feat: solve uppg5

--- uppg5.js
+++ uppg5.js
@@ -1,11 +1,14 @@
-
 function uppg5() {
-    // skriv en if-sats som jämför två tal
-    // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
-    // annars skriv ut "Det andra talet är större än det första"
-
-
-
+  // skriv en if-sats som jämför två tal
+  // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
+  // annars skriv ut "Det andra talet är större än det första"
+  const a = 10;
+  const b = 5;
+  if (a > b) {
+    console.log("Det första talet är större än det andra");
+  } else {
+    console.log("Det andra talet är större än det första");
+  }
 }
 
 module.exports = { uppg5 };
