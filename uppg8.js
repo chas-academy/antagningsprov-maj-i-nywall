3052554b58e463d54eeec1f32c983c1ca421506f
Isak Nywall <nywall@pm.me>
2025-05-08 13:54:31 +0200

    feat: solve uppg8

--- uppg8.js
+++ uppg8.js
@@ -1,17 +1,28 @@
+function uppg8() {
+  // skapa en array med fem person obkjet
+  // varje person objekt ska ha två properties, name och age
 
+  // skapa en fuktion som tar in en array som argument
+  // skapa ett konditionsvillkor som gör att det bara är namnet på personerna som är
+  //  över 30 år som skrivs ut i konsolen
 
-function uppg8(){
+  // anropa funktionen och skicka med arrayen som argument
+  const people = [
+    { name: "Isak", age: 26 },
+    { name: "Alex", age: 31 },
+    { name: "Robert", age: 50 },
+    { name: "Tommy", age: 30 },
+    { name: "Johan", age: 15 },
+  ];
+  function printNamesOfPeopleOver30(people) {
+    people.forEach((person) => {
+      if (person.age > 30) {
+        console.log(person.name);
+      }
+    });
+  }
 
-// skapa en array med fem person obkjet
-// varje person objekt ska ha två properties, name och age
-
-// skapa en fuktion som tar in en array som argument
-// skapa ett konditionsvillkor som gör att det bara är namnet på personerna som är
-//  över 30 år som skrivs ut i konsolen
-
-// anropa funktionen och skicka med arrayen som argument
-
-
+  printNamesOfPeopleOver30(people);
 }
 
 module.exports = { uppg8 };
